d155daf32abb6c59f02145059270f15ffeb817af
joderrick1022 <joderrick1022>
2017-09-27 21:05:25 -0400

    test for equal numbers

--- homework_week1_day3/Program.cs
+++ homework_week1_day3/Program.cs
@@ -10,6 +10,26 @@ namespace homework_week1_day3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("guess a number");
+            int user1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("guess a number");
+            int user2 = int.Parse(Console.ReadLine());
+
+            bool equal = (user1 == user2);
+
+            bool notEqual = (user1 != user2);
+
+            if (equal)
+            {
+                Console.WriteLine("The numbers " + user1 + " and " + user2 + " are the same number!");
+            }
+            else if(notEqual)
+            {
+                Console.WriteLine("The numbers " + user1 + " and" + user2 + " are not equal to each other!");
+            }
+
+
         }
     }
 }
